0eed22b8711ed958af4f4020bea04c68d4333edc
jfcaraujo <jfcaraujo99@gmail.com>
2021-02-20 19:35:55 +0000

    added files to gitignore

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -1,75 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    [SerializeField] private float m_JumpForce = 500f;	
+    [SerializeField] private float m_JumpForce = 500f;
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
-    [SerializeField] private LayerMask m_WhatIsGround;	
-    [SerializeField] private Transform m_GroundCheck;	
+    [SerializeField] private LayerMask m_WhatIsGround;
+    [SerializeField] private Transform m_GroundCheck;
 
     const float k_GroundedRadius = .2f;
     private Rigidbody2D m_Rigidbody2D;
     private Vector3 m_Velocity = Vector3.zero;
     private bool m_FacingRight = false;
     private bool m_Grounded;
-    private bool jump=false;
-    private float horizontalMove=0f;
-    public float runSpeed=40f;
+    private bool jump = false;
+    private float horizontalMove = 0f;
+    public float runSpeed = 40f;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        horizontalMove=Input.GetAxisRaw("Horizontal")*runSpeed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         if (Input.GetButtonDown("Jump"))
-            jump=true;
-
+            jump = true;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         m_Grounded = false;
         Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-		for (int i = 0; i < colliders.Length; i++)
-		{
-			if (colliders[i].gameObject != gameObject)
-			{
-				m_Grounded = true;
+        foreach (var temp_collider in colliders)
+        {
+            if (temp_collider.gameObject != gameObject)
+            {
+                m_Grounded = true;
             }
         }
-        Move(horizontalMove*Time.fixedDeltaTime,jump);
-        jump=false;
+
+        Move(horizontalMove * Time.fixedDeltaTime);
+        jump = false;
     }
 
-    private void Move(float move, bool jump)
+    private void Move(float move)
     {
-        Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        var velocity = m_Rigidbody2D.velocity;
+        Vector3 targetVelocity = new Vector2(move * 10f, velocity.y);
+        m_Rigidbody2D.velocity =
+            Vector3.SmoothDamp(velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
         if (move > 0 && !m_FacingRight)
-		{
-			Flip();
-		}
-		else if (move < 0 && m_FacingRight)
-		{
-			Flip();
-		}     
-        if (m_Grounded && jump)
-		{
-			// Add a vertical force to the player.
-			m_Grounded = false;
-			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-		}  
+        {
+            Flip();
+        }
+        else if (move < 0 && m_FacingRight)
+        {
+            Flip();
+        }
+
+        if (!m_Grounded || !jump) return;
+        // Add a vertical force to the player.
+        m_Grounded = false;
+        m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
     }
 
     private void Flip()
     {
         m_FacingRight = !m_FacingRight;
-        transform.Rotate(0f,180,0f);
+        transform.Rotate(0f, 180, 0f);
     }
 }
