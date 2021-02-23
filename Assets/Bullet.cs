e924195b5f49f5233c4003897b142810240b29cd
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-02-23 17:21:14 +0000

    Add bullet colision

---
+++ Assets/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+        // TODO: use 'other' to see if it is enemy and do stuff
+
+        GameObject effect = Instantiate(hitEffect, gameObject.transform.position, Quaternion.identity);
+        Destroy(effect, 0.6f);
+        Destroy(gameObject);    
+    }
+}
