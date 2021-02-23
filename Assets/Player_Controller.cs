d2c6f761e8f3b52873d1eba21369a8d187cc2002
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-02-23 16:16:08 +0000

    Add gun aiming

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -6,17 +6,23 @@ public class Player_Controller : MonoBehaviour
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
     [SerializeField] private LayerMask m_WhatIsGround;
     [SerializeField] private Transform m_GroundCheck;
+    [SerializeField] private Transform m_Center;
+    [SerializeField] private Transform m_FirePoint;
 
     public Animator animator;
+    public Camera cam;
 
     const float k_GroundedRadius = .2f;
     private Rigidbody2D m_Rigidbody2D;
+    public Transform gun;
     private Vector3 m_Velocity = Vector3.zero;
     private bool m_FacingRight = false;
     private bool m_Grounded;
     private bool jump = false;
     private float horizontalMove = 0f;
     public float runSpeed = 40f;
+
+    private Vector2 mousePos;
 
     // Start is called before the first frame update
     private void Start()
@@ -31,6 +37,8 @@ public class Player_Controller : MonoBehaviour
             jump = true;
 
         animator.SetFloat("HorizontalMove", Mathf.Abs(horizontalMove));
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
@@ -48,7 +56,30 @@ public class Player_Controller : MonoBehaviour
         animator.SetBool("IsJumping", !m_Grounded);
 
         Move(horizontalMove * Time.fixedDeltaTime);
+        Aim();
         jump = false;
+    }
+
+    private void Aim()
+    {
+        Vector2 center = new Vector2(m_Center.position.x, m_Center.position.y);
+
+        Vector2 lookDir = mousePos - center;
+        float lookAngle = -(Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 180f);
+
+        Vector2 gunDir = new Vector2(m_FirePoint.position.x, m_FirePoint.position.y) - center;
+        float gunAngle = -(Mathf.Atan2(gunDir.y, gunDir.x) * Mathf.Rad2Deg - 180f);
+
+        gun.RotateAround(m_Center.position, Vector3.back, lookAngle - gunAngle);
+
+        if ((lookAngle < 85 || lookAngle > 275) && m_FacingRight)
+        {
+            Flip();
+        }
+        else if (lookAngle > 95 && lookAngle < 265 && !m_FacingRight)
+        {
+            Flip();
+        }
     }
 
     private void Move(float move)
@@ -57,14 +88,6 @@ public class Player_Controller : MonoBehaviour
         Vector3 targetVelocity = new Vector2(move * 10f, velocity.y);
         m_Rigidbody2D.velocity =
             Vector3.SmoothDamp(velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-        if (move > 0 && !m_FacingRight)
-        {
-            Flip();
-        }
-        else if (move < 0 && m_FacingRight)
-        {
-            Flip();
-        }
 
         if (!m_Grounded || !jump) return;
         // Add a vertical force to the player.
