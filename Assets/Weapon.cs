f6a563db6019fec63a1e874275ff7c292a95f827
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-02-23 16:59:40 +0000

    Add bullet firing

---
+++ Assets/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firepoint;
+
+    public float bulletForce = 20f;
+
+    // Fire cooldown in seconds
+    public float fireCooldown = 0.4f;
+    private bool allowFire = true;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && allowFire)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        allowFire = false;
+
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(-firepoint.right * bulletForce, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(fireCooldown);
+
+        allowFire = true;
+    }
+}
