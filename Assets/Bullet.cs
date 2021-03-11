d24fc98d2b48e2875f15ef9240d94038061d355b
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-03-11 15:55:28 +0000

    Add paint splatter to ground and obstacles

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject hitEffect;
+    public GameObject splatterPrefab;
 
     private bool exists = true;
 
@@ -16,7 +16,36 @@ public class Bullet : MonoBehaviour
         exists = false;
 
         Vector3 position = gameObject.transform.position;
+
+        // Places paint splatter
+        GameObject splatterObject = Instantiate(splatterPrefab, position, Quaternion.identity, hitInfo.transform);
+
+        SpriteRenderer splatterSR = splatterObject.GetComponent<SpriteRenderer>();
+
+        // Set splatter color to pellet color
+        splatterSR.color = gameObject.GetComponent<SpriteRenderer>().color;
+
+        if (hitInfo.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            // On the ground, the splatter is scaled so it looks angle
+            splatterObject.transform.localScale = Vector3.Scale(splatterObject.transform.localScale, new Vector3(1, 0.75f, 1));
+
+            // Ground splatters are visible on the "Default" sorting layer,
+            // with order 5
+            splatterSR.sortingOrder = 5;
+        }
+        else if (hitInfo.gameObject.layer == LayerMask.NameToLayer("Obstacles"))
+        {
+            // Obstacle splatters are visible on the "Environment" sorting layer,
+            // with order 6
+            splatterSR.sortingLayerName = "Environment";
+            splatterSR.sortingOrder = 6;
+        }
+
+        // Destroys pellet
         Destroy(gameObject);
+
+        // Damage entity (if applicable)
         Enemy_Controller enemy = hitInfo.GetComponent<Enemy_Controller>();
         if (enemy != null)
         {
@@ -27,8 +56,5 @@ public class Bullet : MonoBehaviour
         {
             healthController.Damage(1);
         }
-
-        GameObject effect = Instantiate(hitEffect, position, Quaternion.identity);
-        Destroy(effect, 0.6f);
     }
 }
