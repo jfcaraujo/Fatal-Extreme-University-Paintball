419798f47207f8f6e9c8a268b78aef7ab390f702
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-03-11 21:11:26 +0000

    Implement splatter removal in healing and improve animation

--- Assets/Healing.cs
+++ Assets/Healing.cs
@@ -1,11 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Healing : StateMachineBehaviour
 {
+    List<GameObject> splatters;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        splatters = new List<GameObject>();
+
         animator.gameObject.GetComponent<Player_Controller>().inputBlocked = true;
+
+        Transform legRight = animator.gameObject.transform.Find("LegRight");
+        Transform legLeft = animator.gameObject.transform.Find("LegLeft");
+        Transform body = animator.gameObject.transform.Find("body");
+        Transform head = body.Find("Head");
+
+        foreach (Transform item in new Transform[] { legRight, legLeft, body, head })
+        {
+            for (int i = 0; i < item.childCount; i++)
+            {
+                if (item.GetChild(i).CompareTag("Splatter"))
+                    splatters.Add(item.GetChild(i).gameObject);
+            }
+        }
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        Transform paperTowel = animator.gameObject.transform.Find("body").Find("PaperTowel");
+
+        List<GameObject> splattersToDelete = new List<GameObject>();
+
+        foreach (GameObject splatter in splatters)
+        {
+            if (Mathf.Abs((paperTowel.position - splatter.transform.position).magnitude) < 0.25)
+            {
+                splattersToDelete.Add(splatter);
+                Destroy(splatter);
+            }
+        }
+
+        splatters.RemoveAll(x => splattersToDelete.Contains(x));
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
@@ -13,6 +50,13 @@ public class Healing : StateMachineBehaviour
     {
         animator.gameObject.GetComponent<Player_Controller>().inputBlocked = false;
         animator.gameObject.GetComponent<HealthController>().StopHeal();
-        
+
+        // Remove any lefting splatters
+        foreach (GameObject splatter in splatters)
+        {
+            Destroy(splatter);
+        }
+
+        splatters.Clear();
     }
 }
