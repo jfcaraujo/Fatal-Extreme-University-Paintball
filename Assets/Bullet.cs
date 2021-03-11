3b2acdfb98d37027527b9e18838f975fa2620221
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-03-11 18:28:18 +0000

    Add more splatters
    - Add max distance to pellet
    - Add colliders in environment objects and ground/walls for splatters

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,10 +5,46 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject splatterPrefab;
+    public float maxDistance = 20f;
 
     private bool exists = true;
+    private float travelledDistance = 0f;
+
+    private void Update()
+    {
+        travelledDistance += GetComponent<Rigidbody2D>().velocity.magnitude * Time.deltaTime;
+
+        if (travelledDistance > maxDistance)
+        {
+            RaycastHit2D[] raycasts = Physics2D.RaycastAll(transform.position, Vector3.back);
+
+            List<GameObject> hitObjectsList = new List<GameObject>();
+
+            for (int i = 0; i < raycasts.Length; i++)
+            {
+                if (raycasts[i].transform.gameObject.layer == LayerMask.NameToLayer("EnvironmentObjects"))
+                {
+                    hitObjectsList = new List<GameObject> { raycasts[i].transform.gameObject };
+                    break;
+                }
+
+                if (raycasts[i].transform.gameObject.layer == LayerMask.NameToLayer("BackgroundWall"))
+                {
+                    // BackgroundWall layer is used only for colliders and masks, so we should send the parent
+                    hitObjectsList.Add(raycasts[i].transform.parent.gameObject);
+                }
+            }
+
+            DestroyPellet(hitObjectsList.ToArray());
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        DestroyPellet(new GameObject[] { hitInfo.gameObject });
+    }
+
+    private void DestroyPellet(GameObject[] hitObjectsList)
     {
         if (!exists)
             return;
@@ -17,44 +53,72 @@ public class Bullet : MonoBehaviour
 
         Vector3 position = gameObject.transform.position;
 
-        // Places paint splatter
-        GameObject splatterObject = Instantiate(splatterPrefab, position, Quaternion.identity, hitInfo.transform);
-
-        SpriteRenderer splatterSR = splatterObject.GetComponent<SpriteRenderer>();
-
-        // Set splatter color to pellet color
-        splatterSR.color = gameObject.GetComponent<SpriteRenderer>().color;
-
-        if (hitInfo.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            // On the ground, the splatter is scaled so it looks angle
-            splatterObject.transform.localScale = Vector3.Scale(splatterObject.transform.localScale, new Vector3(1, 0.75f, 1));
-
-            // Ground splatters are visible on the "Default" sorting layer,
-            // with order 5
-            splatterSR.sortingOrder = 5;
-        }
-        else if (hitInfo.gameObject.layer == LayerMask.NameToLayer("Obstacles"))
-        {
-            // Obstacle splatters are visible on the "Environment" sorting layer,
-            // with order 6
-            splatterSR.sortingLayerName = "Environment";
-            splatterSR.sortingOrder = 6;
-        }
-
         // Destroys pellet
         Destroy(gameObject);
 
-        // Damage entity (if applicable)
-        Enemy_Controller enemy = hitInfo.GetComponent<Enemy_Controller>();
-        if (enemy != null)
+        if (hitObjectsList == null || hitObjectsList.Length == 0)
+            return;
+
+        for (int i = 0; i < hitObjectsList.Length; i++)
         {
-            enemy.Damage(1);
-        }
-        HealthController healthController = hitInfo.gameObject.GetComponent<HealthController>();
-        if (healthController != null)
-        {
-            healthController.Damage(1);
+            GameObject hitObject = hitObjectsList[i];
+
+            if (hitObject == null)
+                continue;
+
+            // Places paint splatter
+            GameObject splatterObject = Instantiate(splatterPrefab, hitObject.transform, true);
+            splatterObject.transform.position = position;
+
+            SpriteRenderer splatterSR = splatterObject.GetComponent<SpriteRenderer>();
+
+            // Set splatter color to pellet color
+            splatterSR.color = gameObject.GetComponent<SpriteRenderer>().color;
+
+            if (hitObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                // On the ground, the splatter is scaled so it looks angle
+                splatterObject.transform.localScale = Vector3.Scale(splatterObject.transform.localScale, new Vector3(1, 0.75f, 1));
+
+                // Ground splatters are visible on the "Default" sorting layer,
+                // with order 5
+                splatterSR.sortingOrder = 5;
+            }
+            else if (hitObject.layer == LayerMask.NameToLayer("Obstacles"))
+            {
+                // Obstacle splatters are visible on the "Environment" sorting layer,
+                // with order 6
+                splatterSR.sortingLayerName = "Environment";
+                splatterSR.sortingOrder = 6;
+            }
+            else if (hitObject.layer == LayerMask.NameToLayer("BackgroundWall"))
+            {
+                // Darken paint in wall
+                splatterSR.color *= 0.95f;
+
+                // Wall splatters are visible on the "Default" sorting layer,
+                // with order 4
+                splatterSR.sortingOrder = 4;
+            }
+            else if (hitObject.layer == LayerMask.NameToLayer("EnvironmentObjects"))
+            {
+                // Environment Object splatters are visible on the "Environment" sorting layer,
+                // with order 1
+                splatterSR.sortingLayerName = "Environment";
+                splatterSR.sortingOrder = 1;
+            }
+
+            // Damage entity (if applicable)
+            Enemy_Controller enemy = gameObject.GetComponent<Enemy_Controller>();
+            if (enemy != null)
+            {
+                enemy.Damage(1);
+            }
+            HealthController healthController = gameObject.GetComponent<HealthController>();
+            if (healthController != null)
+            {
+                healthController.Damage(1);
+            }
         }
     }
 }
