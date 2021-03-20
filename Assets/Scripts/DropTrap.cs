72ab7ee3155e5a6ac5f5060e7995f6ae46b08fad
jfcaraujo <jfcaraujo99@gmail.com>
2021-03-20 18:31:45 +0000

    Added prefab for trap

--- Assets/Scripts/DropTrap.cs
+++ Assets/Scripts/DropTrap.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
 public class DropTrap : MonoBehaviour
 {
-    [SerializeField] private bool parentComponent;
     Rigidbody2D m_rigidbody2D;
 
     // Start is called before the first frame update
@@ -14,23 +9,16 @@ public class DropTrap : MonoBehaviour
     {
         m_rigidbody2D = gameObject.GetComponentInChildren<Rigidbody2D>();
     }
-    
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (parentComponent) //activated when rectangle is hit
+        //activated when rectangle is hit
+        if (other.name == "Player")
         {
-            if (other.name == "Player")
-            {
-                m_rigidbody2D.gravityScale = 1.5f;
-                m_rigidbody2D.angularVelocity = 100f;
-                Destroy(gameObject.GetComponent<Collider2D>());
-            }
+            m_rigidbody2D.gravityScale = 1.5f;
+            m_rigidbody2D.angularVelocity = 100f;
+            Destroy(gameObject.GetComponent<Collider2D>());
         }
-        /*else //activated when circle collider (flask itself) is hit
-        {
-            Debug.Log("collision with " + other.name);
-            Bullet.BulletProcessing(other);
-        }*/
     }
 }
