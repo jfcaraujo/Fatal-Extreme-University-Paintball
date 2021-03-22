4a3b4cd64432162cf7bbf2bff78beb16b9073306
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-03-22 20:01:12 +0000

    Add splatter sounds

--- Assets/Scripts/Splatter.cs
+++ Assets/Scripts/Splatter.cs
@@ -2,8 +2,27 @@
 
 public class Splatter : MonoBehaviour
 {
+    public AudioManager audioManager;
+
     private void Start()
     {
         SendMessageUpwards("OnObjectAdded", gameObject, SendMessageOptions.DontRequireReceiver);
+
+        int randomNumber = Random.Range(0, 3);
+
+        switch (Random.Range(0, 3))
+        {
+            case 0:
+                audioManager.PlaySound("Splatter1");
+                break;
+            case 1:
+                audioManager.PlaySound("Splatter2");
+                break;
+            case 2:
+                audioManager.PlaySound("Splatter3");
+                break;
+            default:
+                break;
+        }
     }
 }
